efeb273e6fbc38b535b01d891f6cae9f4bbed1d1
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-10-27 05:43:17 -0500

    feat: added bind

--- app/src/main/java/com/example/shopteamapp/ProductAdapter.kt
+++ app/src/main/java/com/example/shopteamapp/ProductAdapter.kt
@@ -1,11 +1,18 @@
 package com.example.shopteamapp
 
 import Beans.Product
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
 import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONObject
 
 class ProductAdapter(val productList: List<Product>):RecyclerView.Adapter<ProductViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
         var layoutInflater=LayoutInflater.from(parent.context)
         return ProductViewHolder(layoutInflater.inflate(R.layout.item_product,parent,false))
@@ -14,8 +21,12 @@ class ProductAdapter(val productList: List<Product>):RecyclerView.Adapter<Produc
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         val item=productList[position]
         holder.render(item)
+        val listViewItem  = productList[position]
+        holder.productName.text = listViewItem.name
+        holder.bind(productList[position]);
     }
 
     override fun getItemCount(): Int= productList.size
+
 
 }
